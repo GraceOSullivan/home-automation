6282db8880036435315e9481754685e3fe5f7795
Daniel O Sullivan <t00171696@365s.ittralee.ie>
2016-10-25 19:18:36 +0100

    Extracted some functionality (Scanner and displays) from IncreaseTemperature and DecreaseTemperature to the Temperature abstract class to follow DRY principle better

--- src/DecreaseTemperature.java
+++ src/DecreaseTemperature.java
@@ -1,14 +1,10 @@
-import java.util.Scanner;
-
 public class DecreaseTemperature extends Temperature {
     public double changeTemperature(double currentTemperature) {
         displayTemperatureQuery();
 
-        Scanner scanner = new Scanner(System.in);
         double temperature = scanner.nextDouble();
 
-        System.out.println("Decreasing Temperature...");
-        double newTemperature = (currentTemperature - temperature);
-        return newTemperature;
+        displayChangingTemperature();
+        return currentTemperature - temperature;
     }
 }
--- src/Temperature.java
+++ src/Temperature.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public abstract class Temperature {
+    Scanner scanner = new Scanner(System.in);
+
     abstract double changeTemperature(double temperature);
+
     void displayTemperatureQuery(){
         System.out.println("How much would you like to change the temperature by? (°C)");
+    }
+
+    void displayChangingTemperature() {
+        System.out.println("Changing Temperature...");
     }
 }
