66c436b95efbde8da4dbac78e2319a1eb759fa49
Daniel O Sullivan <danielosullivansoftware@gmail.com>
2016-12-03 21:38:07 +0000

    Added a custom XMLReader class that reads values from an XML file, rather than hard-coding them in to the application - WIP

--- src/misc/AppDriver.java
+++ src/misc/AppDriver.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 class AppDriver {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         new Simulator().simulate();
-
     }
 
 
